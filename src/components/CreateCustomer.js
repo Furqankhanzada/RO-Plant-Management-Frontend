3ee338295998a49098481cdd86358e0f47cf1204
zubairkhanzada <zubairkhanzada7@gmail.com>
2019-05-16 13:03:20 +0500

    Form Responsive

--- src/components/CreateCustomer.js
+++ src/components/CreateCustomer.js
@@ -23,16 +23,11 @@ function renderTitle(title) {
                 href="https://www.google.com/search?q=antd"
                 target="_blank"
                 rel="noopener noreferrer"
-            >
+                >
             </a>
         </span>
     );
 }
-
-
-
-
-
 
 class CreateCustomer extends Component {
     constructor(props) {
@@ -87,7 +82,7 @@ class CreateCustomer extends Component {
         const discountObj = {
             percentage: 0,
             product: ''
-        }
+        };
 
         // can use data-binding to get
         discount.push(discountObj);
@@ -96,51 +91,51 @@ class CreateCustomer extends Component {
         })
     };
     removeDiscount = (index) => {
-        alert()
+        alert();
         const { discount } = this.state;
-        discount.splice(index, 1)
+        discount.splice(index, 1);
         this.setState({
             discount
         })
-    }
+    };
     onChangeDiscount = (type, index, ev) => {
-        console.log(ev,'===ev')
+        console.log(ev,'===ev');
         const { discount } = this.state;
         const discountObject = discount[index];
-        console.log(ev, 'evvv')
+        console.log(ev, 'evvv');
         if (type == "percentage") {
             discountObject.discount = ev;
         } else {
             const selectedProduct = JSON.parse(ev);
             this.setState({
                 selectedValue: selectedProduct.name
-            })
-            console.log(selectedProduct,'selectp')
+            });
+            console.log(selectedProduct,'selectp');
             discountObject.product = {
                 name: selectedProduct.name,
                 price: selectedProduct.price
             };
         }
-        discount[index] = discountObject
+        discount[index] = discountObject;
 
         this.setState({
             discount
         })
-    }
+    };
     getCustomerDetails = (ev) => {
         this.setState({
             [ev.target.name]: ev.target.value
         })
-    }
+    };
     handledSubmit = e => {
         e.preventDefault();
 
-        console.log(this.state.discount,'discounts======')
+        console.log(this.state.discount,'discounts======');
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 this.setState({
                     disableBtn: true
-                })
+                });
                 const { name, mobile, password, town, area, block, house, discount } = this.state;
 
                 let customer = {
@@ -160,9 +155,9 @@ class CreateCustomer extends Component {
                             create: discount
                         }
                     }
-                }
+                };
 
-                console.log(customer, '===customer')
+                console.log(customer, '===customer');
                 this.props
                     .createCustomer({
                         variables: customer
@@ -171,13 +166,13 @@ class CreateCustomer extends Component {
                         this.setState({disableBtn: false, name:'', mobile:'', password:'', town:'', area:'', block:'', house:''},() =>
                         {
                             message.success('Customer has been created successfully');
-                        })
+                        });
                         console.log(result, '=====result')
                     })
                     .catch(err => {
                         this.setState({
                             disableBtn: false
-                        })
+                        });
                         const { graphQLErrors } = err;
                         console.log(err, 'errr');
                         // graphQLErrors.forEach(element => {
@@ -219,14 +214,14 @@ class CreateCustomer extends Component {
         })
     };
     render() {
-        console.log(this.state.discount,'dis=====')
+        console.log(this.state.discount,'dis=====');
 
         const { getFieldDecorator, getFieldValue } = this.props.form;
         const SubMenu = Menu.SubMenu;
         const MenuItemGroup = Menu.ItemGroup;
         const { Header, Content, Sider } = Layout;
         const { disableBtn, name, mobile, password, town, area, block, house, selectedValue, discount, result, drawer } = this.state;
-        console.log(drawer,'===drawer==pp')
+        console.log(drawer,'===drawer==pp');
         const children = result.map(email => <Option key={email}>{email}</Option>);
 
         // add field method start here //
@@ -255,7 +250,7 @@ class CreateCustomer extends Component {
                     label={index === 0 ? '' : ''}
                     required={false}
                     key={k}
-                >
+                    >
                     {getFieldDecorator(`names[${k}]`, {
                         validateTrigger: ['onChange', 'onBlur'],
                         rules: [
@@ -271,7 +266,7 @@ class CreateCustomer extends Component {
                             className="dynamic-delete-button"
                             type="minus-circle-o"
                             onClick={() => this.remove(k)}
-                        />
+                            />
                     ) : null}
                 </Form.Item>
                 <Form.Item
@@ -279,7 +274,7 @@ class CreateCustomer extends Component {
                     label={index === 0 ? '' : ''}
                     required={false}
                     key={k}
-                >
+                    >
                     {getFieldDecorator(`names[${k}]`, {
                         validateTrigger: ['onChange', 'onBlur'],
                         rules: [
@@ -295,7 +290,7 @@ class CreateCustomer extends Component {
                             className="dynamic-delete-button"
                             type="minus-circle-o"
                             onClick={() => this.remove(k)}
-                        />
+                            />
                     ) : null}
                 </Form.Item>
 
@@ -307,7 +302,7 @@ class CreateCustomer extends Component {
             <Query query={Products_QUERY}>
                 {({ data, loading }) => {
                     const { products } = data;
-                    console.log(products,'options')
+                    console.log(products,'options');
                     const options = products ? products
                         .map(group => (
                             <Option key={group} value={JSON.stringify(group)}>
@@ -315,7 +310,7 @@ class CreateCustomer extends Component {
                                 <br />
                                 <span>Price: {group.price}</span>
                             </Option>
-                        )) : []
+                        )) : [];
                     return (
                         <Fragment>
 
@@ -324,19 +319,19 @@ class CreateCustomer extends Component {
                                 <Layout className="dashboard-main">
                                     <Sider width={200} style={{ background: '#ffffff', boxShadow: '0 0 28px 0 rgba(24,144,255,.1)' }}>
                                         {/* <Menu
-                             mode="inline"
-                             defaultSelectedKeys={['1']}
-                             defaultOpenKeys={['sub1']}
-                             style={{ height: '100%', borderRight: 0 }}
-                             >
-                             <Menu.Item key="5"><Icon type="team" />Customers</Menu.Item>
-                             </Menu> */}
+                                         mode="inline"
+                                         defaultSelectedKeys={['1']}
+                                         defaultOpenKeys={['sub1']}
+                                         style={{ height: '100%', borderRight: 0 }}
+                                         >
+                                         <Menu.Item key="5"><Icon type="team" />Customers</Menu.Item>
+                                         </Menu> */}
                                         <Menu
                                             onClick={this.handleClick}
                                             mode="inline"
                                             defaultSelectedKeys={['1']}
                                             defaultOpenKeys={['sub1']}
-                                        >
+                                            >
                                             <SubMenu key="sub1" title={<span><Icon type="team" />Customers</span>}>
                                                 <Menu.Item key="1" onClick={() => this.props.history.push('/customers/create')}>Create</Menu.Item>
                                             </SubMenu>
@@ -345,7 +340,7 @@ class CreateCustomer extends Component {
                                     <Layout style={{ padding: '30px 24px 0', height: '100vh' }}>
                                         <div className="create-main-div">
                                             <Row gutter={16}>
-                                                <Col span={8}>
+                                                <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 12 }} xl={{ span: 8 }}>
                                                     <h3>General</h3>
                                                     <Form layout="horizontal">
                                                         <FormItem label={`Mobile Number`} >
@@ -378,7 +373,7 @@ class CreateCustomer extends Component {
                                                         </FormItem>
                                                     </Form>
                                                 </Col>
-                                                <Col span={8}>
+                                                <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 12 }} xl={{ span: 8 }}>
                                                     <h3>Address</h3>
                                                     <Form layout="horizontal">
                                                         <FormItem label={`Town`} >
@@ -421,23 +416,23 @@ class CreateCustomer extends Component {
                                                         </FormItem>
                                                     </Form>
                                                 </Col>
-                                                <Col span={8}>
+                                                <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 12 }} lg={{ span: 12 }} xl={{ span: 8 }}>
                                                     <h3>Discount</h3>
                                                     <Form layout="horizontal">
                                                         {/*<div>
-                                             <InputNumber
-                                             defaultValue={100}
-                                             min={0}
-                                             max={100}
-                                             formatter={value => `${value}%`}
-                                             parser={value => value.replace('%', '')}
-                                             onChange=""
-                                             />
+                                                         <InputNumber
+                                                         defaultValue={100}
+                                                         min={0}
+                                                         max={100}
+                                                         formatter={value => `${value}%`}
+                                                         parser={value => value.replace('%', '')}
+                                                         onChange=""
+                                                         />
 
-                                             <AutoComplete style={{ width: 200 }} onSearch={this.handleSearch} placeholder="input here">
-                                             {children}
-                                             </AutoComplete>
-                                             </div>*/}
+                                                         <AutoComplete style={{ width: 200 }} onSearch={this.handleSearch} placeholder="input here">
+                                                         {children}
+                                                         </AutoComplete>
+                                                         </div>*/}
 
 
 
@@ -451,7 +446,7 @@ class CreateCustomer extends Component {
                                                                                 className="dynamic-delete-button removeButtonDiscount"
                                                                                 type="minus-circle-o"
                                                                                 onClick={this.removeDiscount}
-                                                                            />
+                                                                                />
                                                                             <Form.Item>
 
                                                                                 <AutoComplete
@@ -466,7 +461,7 @@ class CreateCustomer extends Component {
                                                                                     placeholder="Products"
                                                                                     value = {selectedValue}
                                                                                     onChange={this.onChangeDiscount.bind(this, 'product', index)}
-                                                                                >
+                                                                                    >
                                                                                     <Input suffix={<Icon type="search" className="certain-category-icon" />} />
                                                                                 </AutoComplete>
 
@@ -479,7 +474,7 @@ class CreateCustomer extends Component {
                                                                                     formatter={value => `${value}%`}
                                                                                     parser={value => value.replace('%', '')}
                                                                                     onChange={this.onChangeDiscount.bind(this, 'percentage', index)}
-                                                                                />
+                                                                                    />
                                                                             </Form.Item>
                                                                         </div>
                                                                     )
@@ -489,7 +484,7 @@ class CreateCustomer extends Component {
                                                             <Form.Item className="fields-adds" {...formItemLayoutWithOutLabel}>
                                                                 <Button type="dashed" onClick={this.add} style={{ width: '100%' }}>
                                                                     <Icon type="plus" /> Add field
-                                                    </Button>
+                                                                </Button>
                                                             </Form.Item>
                                                         </div>
 
@@ -500,7 +495,7 @@ class CreateCustomer extends Component {
                                                 <Col span={4}>
                                                     <Button type="primary" htmlType="submit" className="login-form-button" onClick={this.handledSubmit} loading={disableBtn}>
                                                         Create
-                                        </Button>
+                                                    </Button>
                                                 </Col>
                                             </Row>
                                         </div>
