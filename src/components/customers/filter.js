fb8284e4b6318ac0239f84abdc6761d2978c081d
zubairkhanzada <zubairkhanzada7@gmail.com>
2019-06-01 12:27:59 +0500

    Implemented Sticky Footer of Create Customer Form

--- src/components/customers/filter.js
+++ src/components/customers/filter.js
@@ -270,6 +270,27 @@ class Filter extends Component {
                         }}
                     </Query>
 
+                    <div
+                        style={{
+                          position: 'absolute',
+                          left: 0,
+                          bottom: 0,
+                          width: '100%',
+                          borderTop: '1px solid #e9e9e9',
+                          padding: '10px 16px',
+                          background: '#fff',
+                          textAlign: 'right',
+                        }}
+                        >
+                        <Button onClick={this.onClose} style={{ marginRight: 8 }}>
+                            Cancel
+                        </Button>
+                        <Button onClick={this.onClose} type="primary">
+                            <Icon type="plus" />
+                            Create
+                        </Button>
+                    </div>
+
                 </Drawer>
 
 
